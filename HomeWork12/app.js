b796122e00d5b1885a0eea79540248d375e77033
Omega-Nine <oleksandrnesterenko67@gmail.com>
2026-08-13 18:59:36 +0300

    Feat: add book list navigation and preview rendering.

--- HomeWork12/app.js
+++ HomeWork12/app.js
@@ -3,6 +3,8 @@ const books = JSON.parse(savedBooks);
 
 const bookList = document.createElement("ul");
 
+
+
 books.forEach(function(item) {
     const li = document.createElement("li");
 
@@ -16,8 +18,33 @@ books.forEach(function(item) {
 
     li.addEventListener("click", function() {
         history.pushState(null, "", `?id=${item.id}#preview`);
-        console.log(window.location.hash);
+
         const url = new URL(window.location.href);
+        const id = url.searchParams.get("id");
+        const bookId = Number(id);
+        const selectedBook = books.find(function(book) {
+            return book.id === bookId;
+        });
+        console.log(selectedBook);
+
+        const bookName = document.createElement("h2");
+        bookName.textContent = selectedBook.bookname;
+
+        const previewSection = document.createElement("div");
+        previewSection.append(bookName);
+
+        const bookAuthor = document.createElement("p");
+        bookAuthor.textContent = selectedBook.author;
+        previewSection.append(bookAuthor);
+
+        const bookImage = document.createElement("img");
+        bookImage.src = selectedBook.image;
+        previewSection.append(bookImage);
+
+        const bookPlot = document.createElement("p");
+        bookPlot.textContent = selectedBook.plot;
+        previewSection.append(bookPlot);
+        document.body.append(previewSection);
     });
 
     editButton.addEventListener("click", function(event) {
